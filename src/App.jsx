72211bd3692fdaa7dc5774f648d2dcff65b711a8
Lucien KOHLI <lucien.kohli@gmail.com>
2023-12-18 23:55:58 +0100

    FirstVersion Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Link, Outlet } from "react-router-dom";
 
 import "./App.css";
 import Header from "./components/Header";
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <main>
         <Outlet />
       </main>
+      <Carousel />
     </>
   );
 }
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import Slider from "react-slick";
+import "../components/Carousel.css";
+import Img1 from "../assets/Img1.png";
+import Img2 from "../assets/img2.png";
+import Img3 from "../assets/img3.png";
+import Img4 from "../assets/img4.png";
+import Img5 from "../assets/img5.png";
+import Img6 from "../assets/img6.png";
+import Img7 from "../assets/img7.png";
+import Img8 from "../assets/img8.png";
+import Img9 from "../assets/img9.png";
+import Img10 from "../assets/img10.png";
+import Img11 from "../assets/img11.png";
+import Img12 from "../assets/img12.png";
+import Img13 from "../assets/img13.png";
+import Img14 from "../assets/img14.png";
+import Img15 from "../assets/img15.png";
+import Img16 from "../assets/img16.png";
+import Img17 from "../assets/img17.png";
+import Img18 from "../assets/img18.png";
+import Img19 from "../assets/img19.png";
+import Img20 from "../assets/img20.png";
+
+const images = [
+  Img1,
+  Img2,
+  Img3,
+  Img4,
+  Img5,
+  Img6,
+  Img7,
+  Img8,
+  Img9,
+  Img10,
+  Img11,
+  Img12,
+  Img13,
+  Img14,
+  Img15,
+  Img16,
+  Img17,
+  Img18,
+  Img19,
+  Img20,
+];
+
+function SampleNextArrow({ onClick }) {
+  return (
+    <div className="arrow arrow-right" onClick={onClick}>
+      <BsArrowRight />
+    </div>
+  );
+}
+
+function SamplePrevArrow({ onClick }) {
+  return (
+    <div className="arrow arrow-left" onClick={onClick}>
+      <BsArrowLeft />
+    </div>
+  );
+}
+
+const Carousel = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const settings = {
+    infinite: true,
+    dots: true,
+    speed: 500,
+    slidesToShow: 3,
+    centerMode: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    beforeChange: (current, next) => setSlideIndex(next),
+  };
+  return (
+    <div className="container">
+      <h2 className="SlideHead"> Characters Carousel </h2>
+      <div className="slider">
+        <Slider {...settings}>
+          {images.map((image, index) => (
+            <div
+              className={index === slideIndex ? "slide slide-active" : "slide"}
+              key={index}
+            >
+              <img src={image} alt="" />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
