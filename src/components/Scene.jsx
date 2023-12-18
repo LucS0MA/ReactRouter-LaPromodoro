fb50a90c12145626723e096aaf6a2fb2b8d41add
Maeva Fauvel <mae.fauvel@gmail.com>
2023-12-18 20:21:49 +0100

    Import Yoda3D from Spline done

---
+++ src/components/Scene.jsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+function Scene(){
+    return <div>
+        <Spline scene="https://prod.spline.design/J1HttBjmmHLlyum5/scene.splinecode" />
+    </div>
+}
+
+export default Scene;
