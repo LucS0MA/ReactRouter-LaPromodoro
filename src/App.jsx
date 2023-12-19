ae29f2af2e68d985daadbafc085af21ee846ab5e
gatsuweb <ivan.duran@outlook.fr>
2023-12-19 18:07:52 +0100

    modif add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Jeu from './pages/Jeu.jsx';
 import DuelPage from './pages/DuelPage.jsx'; // Importez votre composant Duel ici
