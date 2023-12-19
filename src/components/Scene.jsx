e338f87c4a23380f7cbd4109e55acbe92ac5d442
Maeva Fauvel <mae.fauvel@gmail.com>
2023-12-19 01:30:03 +0100

    Baby Yoda added

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,7 +1,8 @@
 import Spline from '@splinetool/react-spline';
+import "./Scene.css";
 
 function Scene(){
-    return <div>
+    return <div className='sceneSection'>
         <Spline scene="https://prod.spline.design/J1HttBjmmHLlyum5/scene.splinecode" />
     </div>
 }
