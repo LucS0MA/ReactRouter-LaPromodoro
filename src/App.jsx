1ab02498896a8ba304872708c87916c3a650ccc4
gatsuweb <ivan.duran@outlook.fr>
2023-12-19 17:54:07 +0100

    parallax001A

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       <main>
         <Outlet />
       </main>
+      
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,16 @@
+import './Home.css'
+import ComponentParallax from '../components/ComponentParallax';
 function Home() {
-  return <div>page Home Poulet</div>;
+  return (
+    <>
+  
+  <div>page Home Poulet</div>,
+  
+  <div>
+        <ComponentParallax />
+  </div>
+   <h1 style={{fontSize: '100px', zIndex:2}}>Poulchat</h1> 
+   </>
+  )
 }
 export default Home;
